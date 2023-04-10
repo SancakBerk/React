1c98f3c8b5569ff013a06e716da34d0e93073015
Berk Sancak <98399409+SancakBerk@users.noreply.github.com>
2023-04-10 22:27:06 +0300

    Delete Api data images directory

--- Api data images/components/imageitem.jsx
+++
@@ -1,9 +0,0 @@
-function ImageItem({image}) {
-    return ( 
-    <div>
-        <img src={image.urls.small} alt={image.alt_description} className="imgItem" />
-    </div>
-    );
-}
-
-export default ImageItem;
